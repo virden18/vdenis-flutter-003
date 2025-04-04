96aba3d84a6dad8e70acdca12fe5eb9192308114
virden18 <virgilio.denis18@gmail.com>
2025-04-04 10:37:07 -0300

    Stateless vs. Stateful Widgets
    Se creó la pantalla 'MiPantalla' como un StatefulWidget, implementando
    un contador que se incrementa al presionar un botón.
    
    Uso del setState para actualizar el estado de la pantalla.
    
    Se configuró el archivo main.dart para incluir la nueva pantalla.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vdenis_flutter_003/views/mi_pantalla.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,47 +17,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: const MiPantalla(),
-    );
-  }
-}
-
-class MiPantalla extends StatelessWidget {
-  const MiPantalla({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mi App'),
-      ),
-      body: Center(
-        // Column organiza los widgets en una columna vertical
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            /*
-            Containter
-            El containter envuelve al texto con un fondo, separando las partes del widget
-            haciendo que todos los cambios del container se apliquen solo dentro de él.
-            Puede almacenar otros widgets dentro de él.
-            */
-            Container(
-              padding: EdgeInsets.all(20), // Modificación 2: Agrega un padding de 20
-              color: Colors.green, // Modificación 1: Cambia el color de fondo
-              child: const Text(
-                'Hola, Flutter',
-                style: TextStyle(fontSize: 24),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-              },
-              child: const Text('Toca aquí'), // Modificación 3: Cambia el texto del botón
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/views/mi_pantalla.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class MiPantalla extends StatefulWidget {
+  const MiPantalla({super.key});
+
+  @override
+  MiPantallaState createState() => MiPantallaState();
+}
+
+class MiPantallaState extends State<MiPantalla> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    /*
+    Usamos setState para notificar a Flutter que el estado del widget 
+    ha cambiado y que debe reconstruir la interfaz.
+  
+    Sin setState, los cambios en el estado no se reflejarán en la 
+    pantalla, lo que puede causar una experiencia de usuario inconsistente.
+    */
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mi App')),
+      body: Center(
+        // Column organiza los widgets en una columna vertical
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            /*
+              Containter
+              El containter envuelve al texto con un fondo, separando las partes del widget
+              haciendo que todos los cambios del container se apliquen solo dentro de él.
+              Puede almacenar otros widgets dentro de él.
+              */
+            Container(
+              padding: EdgeInsets.all(20), // Agrega un padding de 20
+              color: Colors.green, // Cambia el color de fondo
+              child: const Text(
+                'Hola, Flutter',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Veces presionado: $_counter', // Modificación 1: Cambia el texto del contador
+              style: TextStyle( 
+                fontSize: 20,
+                // Modificación 3: Cambia el color del texto
+                color: Colors.blue), 
+            ),
+            const SizedBox(height: 30), // Modificación 2: Cambia el tamaño del espacio entre el texto y el botón
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: const Text(
+                'Toca aquí',
+              ), // Cambia el texto del botón
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
